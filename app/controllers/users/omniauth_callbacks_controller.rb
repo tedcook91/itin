1e5e938255cb3c6eae6061d161273f2853c35305
captainangus <27704548+CaptainAngus@users.noreply.github.com>
2017-12-05 11:21:45 -0500

    omniauth fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,17 +1,17 @@
-# class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-#   protect_from_forgery unless: -> { request.format.json? }
-#
-#   def github
-#     @user = User.from_omniauth(request.env["omniauth.auth"])
-#
-#     if @user.persisted?
-#       sign_in_and_redirect @user, :event => :authentication
-#     else
-#       redirect_to root_path
-#     end
-#   end
-#
-#   def failure
-#     redirect_to root_path
-#   end
-# end
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  protect_from_forgery unless: -> { request.format.json? }
+
+  def github
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      redirect_to root_path
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
+end
